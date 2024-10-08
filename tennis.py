df39448215b23deb619ce1faba007a67157c1876
Adithya Laakso <HugoHead@github.com>
2024-10-08 08:23:29 -0400

    Reads from csv files

--- tennis.py
+++ tennis.py
@@ -1,7 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+import csv
+
 from matplotlib.widgets import Slider
 from mpl_toolkits.mplot3d import Axes3D
+
+# Function to extract a column
+def get_column(matrix, col_index):
+    return [row[col_index] for row in matrix]
+
+#import csv
+data = []
+with open('Newshoulderdata.txt', 'r') as file:
+    reader = csv.reader(file, delimiter = ' ')
+    for row in reader:
+        cleaned_row = [float(value) for value in row if value]
+        data.append(cleaned_row)
+
+
+x = np.arange(len(data))
+y = [point[1] for point in data]  # Extracting the second column for y values
 
 # Set up the figure and 3D axes
 fig = plt.figure()
@@ -15,9 +34,8 @@ wrist_angle = 0
 # Function to draw the player
 def draw_player(shoulder_angle, elbow_angle, wrist_angle):
     ax.cla()  # Clear the axes
-
-    # Define joint positions based on angles
     shoulder = np.array([0, 0, 0])
+
     elbow = shoulder + np.array([np.cos(np.radians(shoulder_angle)),
                                  np.sin(np.radians(shoulder_angle)),
                                  0])
@@ -45,30 +63,35 @@ def draw_player(shoulder_angle, elbow_angle, wrist_angle):
     ax.set_zlabel('Z')
     ax.set_title('3D Tennis Player Animation')
 
-# Initial draw
-draw_player(shoulder_angle, elbow_angle, wrist_angle)
-
 # Slider setup
 axcolor = 'lightgoldenrodyellow'
-ax_slider_shoulder = plt.axes([0.1, 0.01, 0.65, 0.03], facecolor=axcolor)
 ax_slider_elbow = plt.axes([0.1, 0.05, 0.65, 0.03], facecolor=axcolor)
 ax_slider_wrist = plt.axes([0.1, 0.09, 0.65, 0.03], facecolor=axcolor)
 
-slider_shoulder = Slider(ax_slider_shoulder, 'Shoulder Angle', -180, 180, valinit=shoulder_angle)
 slider_elbow = Slider(ax_slider_elbow, 'Elbow Angle', -90, 90, valinit=elbow_angle)
 slider_wrist = Slider(ax_slider_wrist, 'Wrist Angle', -90, 90, valinit=wrist_angle)
 
+plt.ion()
+plt.show()
+
+i = [0]
+
 # Update function for sliders
 def update(val):
-    shoulder_angle = slider_shoulder.val
     elbow_angle = slider_elbow.val
     wrist_angle = slider_wrist.val
-    draw_player(shoulder_angle, elbow_angle, wrist_angle)
-    plt.draw()
 
-# Connect sliders to the update function
-slider_shoulder.on_changed(update)
 slider_elbow.on_changed(update)
 slider_wrist.on_changed(update)
 
-plt.show()
+firstShoulderPos = data[i[0]]
+
+# Initial draw
+draw_player(data[i[0]][0], elbow_angle, wrist_angle)
+
+while True:
+    i[0] += 1
+    print(data[i[0]])
+    draw_player(data[i[0]][0], elbow_angle, wrist_angle)
+    plt.draw()
+    plt.pause(0.1)
