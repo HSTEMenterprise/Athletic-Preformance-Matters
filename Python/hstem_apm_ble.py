0fbc00a6ebc9046c9528c2ea43979881c2586521
ssbar <ssdunbar@mtu.edu>
2026-02-09 18:04:49 -0500

    3 Concurrent connections
    Continued work to change function to accept 3 simultaneous calls from asyncio.gather(....)

--- Python/hstem_apm_ble.py
+++ Python/hstem_apm_ble.py
@@ -1,12 +1,13 @@
 '''
 Author: Oliver 
 Date: 11/16/25
-Last Updated: 11/16/25
+Last Updated: 2/9/26
 Purpose: Connecting to the ESP32 via Blutooth and saving data to a csv file
 '''
 
 import asyncio
 import csv
+from datetime import date
 from bleak import BleakClient, BleakScanner
 import uuid
 import sys
@@ -26,14 +27,14 @@ CSV_FILE_NAME = ""
 # 3 columns for each sensor organized: Shoulder elbow wrist
 def create_new_csv_file() -> str:
     """Create a fresh CSV file and return a unique filename."""
-    name = f"xyz-data-{uuid.uuid1().hex}.csv"
-    field_names = ["x", "y", "z"]
+    name = f"xyz-data-{date.today().strftime('%Y-%m-%d')}.csv"
+    field_names = ["shoulderx", "shouldery", "shoulderz", "elbowx", "elbowy", "elbowz", "wristx", "wristy", "wristz"]
     with open(name, "w", newline="") as f:
         writer = csv.DictWriter(f, fieldnames=field_names)
         writer.writeheader()
     return name
     
-def append_to_csv_file(file_name, data):
+def append_to_csv_file(file_name, data, columnName):
     with open(file_name, "a") as f:
         writer = csv.writer(f, delimiter=",")
         writer.writerow(data)
@@ -42,21 +43,19 @@ def append_to_csv_file(file_name, data):
 MAX_DATAPOINTS = 100
 NUM_COLLECTED = 0
 
-async def main():
+async def sensorComms(uuid, c_uuid, body_part):
     global NUM_COLLECTED
-    print(f"INFO: Scanning for {DEVICE_NAME}...")
+    print(f"INFO: Scanning for {body_part} sensor...")
     devices = await BleakScanner.discover(return_adv=True)
     esp32 = None
     for d, a in devices.values():
-        if a.service_uuids and a.service_uuids[0] == SERVICE_UUID:
+        if a.service_uuids and a.service_uuids[0] == uuid:
             esp32 = d
             break
 
     if not esp32:
-        print(f"ERROR: {DEVICE_NAME} not found.")
+        print(f"ERROR: {body_part} sensor not found.")
         return
-        
-    CSV_FILE_NAME = create_new_csv_file()
 
     print(f"INFO: Connecting to {esp32.address}...")
     async with BleakClient(esp32.address) as client:
@@ -74,10 +73,22 @@ async def main():
             append_to_csv_file(CSV_FILE_NAME, decoded_data.split(" "))  
             NUM_COLLECTED += 1
 
-        await client.start_notify(CHARACTERISTIC_UUID, handle_notification)
+        await client.start_notify(c_uuid, handle_notification)
         print("=== Receiving data... (press Ctrl+C to stop) ===")
         await asyncio.sleep(60)
-        await client.stop_notify(CHARACTERISTIC_UUID)
+        await client.stop_notify(c_uuid)
+
+
+async def main():
+    global CSV_FILE_NAME = 
+    CSV_FILE_NAME = create_new_csv_file()
+
+    ## Run our bluetooth function with asyncio.gather(3 times!) with shoulder, elbow, and wrist UUIDs and c UUID's and also which body part it is
+    await asyncio.gather(
+        sensorComms(SHOULDER_UUID, SHOULDER_C_UUID, "shoulder"),
+        sensorComms(ELBOW_UUID, ELBOW_C_UUID, "elbow"),
+        sensorComms(WRIST_UUID, WRIST_C_UUID, "wrist")
+    )
 
 if __name__ == "__main__":
     asyncio.run(main())
